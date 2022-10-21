f0cb82e2c01b2a517d7b6b0ed567410683ec435e
yutent <yutent.io@gmail.com>
2022-10-21 14:22:27 +0800

    fixed windows path

--- index.js
+++ index.js
@@ -13,15 +13,17 @@ import createServer from './lib/dev.js'
 import compile from './lib/prod.js'
 
 const WORK_SPACE = process.cwd()
+const IS_WINDOWS = process.platform === 'win32'
 
 const CONFIG_FILE = join(WORK_SPACE, 'vue.live.js')
 const SOURCE_DIR = join(WORK_SPACE, 'src')
+const PROTOCOL = IS_WINDOWS ? 'file://' : ''
 
 let args = process.argv.slice(2)
 
 switch (args[0]) {
   case 'dev':
-    import(CONFIG_FILE)
+    import(PROTOCOL + CONFIG_FILE)
       .then(function (conf) {
         createServer(SOURCE_DIR, conf.default)
       })
@@ -31,7 +33,7 @@ switch (args[0]) {
     break
 
   case 'build':
-    import(CONFIG_FILE)
+    import(PROTOCOL + CONFIG_FILE)
       .then(function (conf) {
         let dist = conf.buildDir || 'dist'
         if (fs.isdir(dist)) {
